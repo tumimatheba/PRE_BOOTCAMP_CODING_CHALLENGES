d1a8aad3d4e0f04e526a4c16c62822602fd7d0da
Itu Matheba <tumimatheba@gmail.com>
2020-11-30 08:58:43 +0200

    clean up code

--- Task10.js
+++ Task10.js
@@ -1,12 +1,12 @@
 function strVowels(str) {
     var vowels = "AEIOUaeiou";
-    var result = [];
+    var vowelResult = [];
 
     for (var i = 0; i < str.length; i++) {
        if (vowels.indexOf(str[i]) !== -1) {
-        result.push(str[i]);
+       vowelResult.push(str[i]);
        }
     }
-    return result;
+    return vowelResult;
 }
 
